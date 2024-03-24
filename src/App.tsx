0e03e615ae0f1c84961af8f6322e5439b94cf365
Xu Yinjie <xuyinjie@xiaobangtouzi.com>
2024-03-24 16:44:59 +0800

    feat: 增加less antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Game } from 'src/page/game';
+
+import './App.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' Component={Game}>
+          <Route path='/game/:id' Component={Game}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
