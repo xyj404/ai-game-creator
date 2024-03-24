0ef3365dc39f09334024a89594b4de75fd2ce19d
Xu Yinjie <xuyinjie@xiaobangtouzi.com>
2024-03-24 17:21:51 +0800

    feat: 完善页面布局

---
+++ src/share/component/avatar/index.tsx
@@ -0,0 +1,12 @@
+import { Input } from 'antd';
+import React from 'react';
+
+import './index.less';
+
+export const Dialog = () => {
+  return (
+    <div className={'dialog-wrapper'}>
+      <Input></Input>
+    </div>
+  );
+};
